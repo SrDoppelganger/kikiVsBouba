efecf9d853e42bbc782507d1fdd4202db8b6a15a
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-07-08 19:48:50 -0300

    started working on the state machine

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,7 @@
 extends CharacterBody2D
 
+@onready var state_machine: StateMachine = $"State Machine";
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _init() -> void: state_machine.init();
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _process(delta: float) -> void: state_machine.process_frame(delta);
