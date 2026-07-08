d6536c892400a96f0a8b32f17039589ee0fe1af6
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-07-08 20:05:03 -0300

    added idle state

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,13 @@
+class_name Player
 extends CharacterBody2D
 
 @onready var state_machine: StateMachine = $"State Machine";
+@onready var animation: AnimationPlayer = $Animation
 
-func _init() -> void: state_machine.init();
+func _ready() -> void: state_machine.init();
 
 func _process(delta: float) -> void: state_machine.process_frame(delta);
+
+func _physics_process(delta: float) -> void: state_machine.process_physics(delta);
+	
+func _input(event: InputEvent) -> void: state_machine.process_input(event);
